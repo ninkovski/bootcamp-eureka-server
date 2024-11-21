ecc66312931c625627e40d74ef1a9569f6f7cdad
federico1978 <femero1978@hotmail.com>
2024-11-20 19:06:53 -0500

    Se agrega el proyecto

---
+++ src/test/java/com/bootcamp/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.eurekaserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
